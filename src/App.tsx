0dde0ce13622587f6ed44f942331a2e32cf1bd37
GuillermoLL <guillermo.jobs@hotmail.com>
2023-05-15 23:24:13 +0200

    estructura basica y componente list

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-function App(): any {
-	return <h1>App its running</h1>
-}
-
-export default App
---
+++ src/components/app/App.tsx
@@ -0,0 +1,13 @@
+import List from "./List/List.component"
+
+export default function App(): any {
+	return (
+		<>
+			<h1>App</h1>
+			<List></List>
+			<List></List>
+			<List></List>
+			<List></List>
+		</>
+	)
+}
